0918fbadce0be70634e0349589905bd946be6194
hiten2110 <hitensharma215@gmail.com>
2024-07-26 21:58:33 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Playing play.greenmc.fun","discord.gg/greenmc","Playing greenmc.in"];
+const statusMessages = ["Playing greenmc.in"];
 
 
 let currentIndex = 0;
@@ -73,7 +73,7 @@ function updateStatusAndSendMessages() {
   const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
 
   client.user.setPresence({
-    activities: [{ name: currentStatus, type: ActivityType.Custom}],
+    activities: [{ name: currentStatus, type: ActivityType.Playing}],
     status: 'dnd',
   });
 
